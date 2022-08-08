0421878890da937ab0f323f1c56f6d8fd7d31feb
kebishaa <kbkibatu@gmail.com>
2022-08-08 16:14:13 +0300

    added find_source

--- extract_dataframe.py
+++ extract_dataframe.py
@@ -66,4 +66,14 @@ class TweetDfExtractor:
         for time in self.tweets_list:
             created_at.append(time['created_at'])
         return created_at
+    def find_created_time(self) -> list:
+        created_at = []
+        for time in self.tweets_list:
+            created_at.append(time['created_at'])
+        return created_at
+    def find_source(self) -> list:
+        source = []
+        for x in self.tweets_list:
+            source.append(x['source'])
+        return source
          
