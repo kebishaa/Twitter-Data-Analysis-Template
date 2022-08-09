920a428acd78bd25bd5521da9ede76cbc5e6a4ff
kebishaa <kbkibatu@gmail.com>
2022-08-09 15:47:34 +0300

    defined init function

---
+++ clean_tweets_dataframe.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import extract_dataframe as ed
+
+
+class Clean_Tweets:
+    """
+    The PEP8 Standard AMAZING!!!
+    """
+
+    def __init__(self, df: pd.DataFrame):
+        self.df = df
+        print('Automation in Action...!!!')
