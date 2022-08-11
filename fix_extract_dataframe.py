57e66c709d1d0231fc55a9fb60c9ce77f28b771d
kebishaa <kbkibatu@gmail.com>
2022-08-11 14:05:25 +0300

    modified extract dataframe

--- fix_extract_dataframe.py
+++ fix_extract_dataframe.py
@@ -4,16 +4,7 @@ from textblob import TextBlob
 
 
 def read_json(json_file: str)->list:
-    """
-    json file reader to open and read json files into a list
-    Args:
-    -----
-    json_file: str - path of a json file
     
-    Returns
-    -------
-    length of the json file and a list of json
-    """
     
     tweets_data = []
     for tweets in open(json_file,'r'):
